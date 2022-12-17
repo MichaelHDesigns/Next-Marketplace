a1f3bae97342a30af99287e22606618db9232653
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2022-12-16 20:47:27 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 2330
     },
     mumbai: {
       // maticvigil mumbai, you can use any other RPC u want.
